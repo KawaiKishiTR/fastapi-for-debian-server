df779e465592eb6f2f90f5fd713bfc51f6f7068c
KawaiKishiTR <aselafen@gmail.com>
2025-12-28 01:08:31 +0300

    sweet server added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.routers import mc_survival, mc_redstone, fc_vanilla, fc_krastorio2, root, servers, machine
+from app.routers import mc_survival, mc_redstone, mc_sweet, fc_vanilla, fc_krastorio2, root, servers, machine
 from app.lifespan import lifespan
 
 
@@ -10,12 +10,15 @@ app = FastAPI(lifespan=lifespan)
 # Static
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+app.mount("/files", StaticFiles(directory="files"), name="files")
 
 # Routers
 app.include_router(root.router)
 app.include_router(machine.router, prefix="/machine")
 app.include_router(servers.router, prefix="/servers")
+app.include_router(mc_sweet.router, prefix="/servers/mc-sweet")
 app.include_router(mc_survival.router, prefix="/servers/mc-survival")
+app.include_router(mc_redstone.router, prefix="/servers/mc-redstone")
 app.include_router(mc_redstone.router, prefix="/servers/mc-redstone")
 app.include_router(fc_vanilla.router, prefix="/servers/fc-vanilla")
 app.include_router(fc_krastorio2.router, prefix="/servers/fc-krastorio2")
