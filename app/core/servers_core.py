e85cf3d751b2405b183c1ff4a06a64e03b557dfe
KawaiKishiTR <aselafen@gmail.com>
2026-01-26 01:31:14 +0300

    mc-forgepack added

--- app/core/servers_core.py
+++ app/core/servers_core.py
@@ -5,7 +5,7 @@ class ServerID(Enum):
     MC_SURVIVAL = "mc-survival"
     MC_REDSTONE = "mc-redstone"
     MC_SWEET = "mc-sweet"
-
+    MC_FORGEPACK = "mc-forgepack"
 
     FC_VANILLA = "fc-vanilla"
     FC_KRASTORIO2 = "fc-krastorio2"
