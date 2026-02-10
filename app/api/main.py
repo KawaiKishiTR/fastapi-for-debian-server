e4246c10586f296edd1e7f9ad159c3bf96ed9d47
KawaiKishiTR <aselafen@gmail.com>
2026-02-11 02:57:45 +0300

    apoch endpoint added

--- app/api/main.py
+++ app/api/main.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+import time
+from fastapi import APIRouter, Response
 from ..api import servers_endpoints
 from ..api import machine
 
@@ -6,3 +7,12 @@ router = APIRouter()
 
 router.include_router(machine.router, prefix="/machine")
 router.include_router(servers_endpoints.router, prefix="/servers")
+
+
+
+@router.get("/epoch")
+def get_epoch():
+    return Response(
+        content=str(int(time.time())),
+        media_type="text/plain"
+    )
